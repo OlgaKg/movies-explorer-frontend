dffc7ea9555ddbf328c5ffa74aa502782b3cac79
OlgaKG <flory_oli@mail.ru>
2023-10-18 13:27:49 +0200

    500

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Выбранный макет light-4
 
 Ссылка на пул реквест: 
+https://github.com/OlgaKg/movies-explorer-frontend/tree/level-3
 
 Ссылку на задеплоенный на сервере проект: 
 https://dpmovie.nomoredomainsicu.ru/
--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -79,8 +79,8 @@ class MainApi {
 }
 
 const mainApi = new MainApi({
-    baseUrl: 'http://localhost:4000',
-    // baseUrl: 'https://api.ypmesto.nomoreparties.co',
+    // baseUrl: 'http://localhost:3002',
+    baseUrl: 'https://api.dpmovie.nomoredomainsicu.ru',
     headers: {
         'Content-Type': 'application/json'
     }
